3de49d6140392eccfba6d1431f36fe8dba7c9345
dianamorenodev <valentinamoreno2002@gmail.com>
2023-02-13 11:36:08 -0500

    cards

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -7,31 +7,31 @@ const Proyects = () => {
   return (
     <Container id='Proyects'>
       <Row>
-        <Col className='col-lg-12 col-md-12 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
+        <Col className='col-lg-12 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
           <h2>My Projects</h2>
         </Col>
       </Row>
       <Row>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <Card id='proyects'>
+          <Card id='proyects' style={{ width: '20rem' }}>
             <Card.Img variant="cover" src={BadJokes} />
           </Card>
         </Col>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <Card id='proyects'>
+          <Card id='proyects' style={{ width: '20rem' }}>
             <Card.Img variant="cover" src={Cripto} />
           </Card>
         </Col>
       </Row>
       <Row>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <Card id='proyects'>
-
+          <Card id='proyects' style={{ width: '20rem' }}>
+            <Card.Img variant="cover" src={BadJokes} />
           </Card>
         </Col>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <Card id='proyects'>
-
+          <Card id='proyects' style={{ width: '20rem' }}>
+            <Card.Img variant="cover" src={BadJokes} />
           </Card>
         </Col>
       </Row>
