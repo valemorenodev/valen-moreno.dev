b28f5a11c4d1a3710d3be2b38b194e0584882e98
dianamorenodev <valentinamoreno2002@gmail.com>
2023-02-14 12:44:24 -0500

    link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <Container id='Footer'>
       <Row>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <h3>valemorenodev</h3>
+          <a href="https://github.com/valemorenodev" target={'_blank'}><h3>valemorenodev</h3></a>
         </Col>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
           <h4><a href="https://www.linkedin.com/in/diana-valentina-moreno/" target={'_blank'}><span><img src={LinkedIn} /></span></a></h4>
