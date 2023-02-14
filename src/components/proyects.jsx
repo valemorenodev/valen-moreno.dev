a1c3fe589d837779eccd2b8083ece9611cc15b18
dianamorenodev <valentinamoreno2002@gmail.com>
2023-02-14 13:28:55 -0500

    update proyects

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -16,7 +16,7 @@ const Proyects = () => {
       <Row>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
           <a href="https://valemorenodev.github.io/bad-jokes/" target={'_blank'}>
-            <Card id='proyects' style={{ width: '23rem' }}>
+            <Card id='proyects' style={{ width: '20rem' }}>
               <Card.Img src={BadJokes} />
               <Card.ImgOverlay id='proyectsTxt'>
                 <h2>BadJokes</h2>
@@ -26,7 +26,7 @@ const Proyects = () => {
         </Col>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
           <a href="http://cripto-eosin.vercel.app/" target={'_blank'}>
-            <Card id='proyects' style={{ width: '23rem' }}>
+            <Card id='proyects' style={{ width: '20rem' }}>
               <Card.Img src={Cripto} />
               <Card.ImgOverlay id='proyectsTxt'>
                 <h2>Cripto</h2>
@@ -37,20 +37,24 @@ const Proyects = () => {
       </Row>
       <Row>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <Card id='proyects' style={{ width: '23rem' }}>
-            <Card.Img src={RickAndMorty} />
-            <Card.ImgOverlay id='proyectsTxt'>
-              <h2>Rick & Morty</h2>
-            </Card.ImgOverlay>
-          </Card>
+          <a href="https://rick-and-morty-mocha-delta.vercel.app/" target={'_blank'}>
+            <Card id='proyects' style={{ width: '20rem' }}>
+              <Card.Img src={RickAndMorty} />
+              <Card.ImgOverlay id='proyectsTxt'>
+                <h2>Rick & Morty</h2>
+              </Card.ImgOverlay>
+            </Card>
+          </a>
         </Col>
         <Col className='col-lg-6 col-md-6 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center'>
-          <Card id='proyects' style={{ width: '23rem' }}>
-            <Card.Img src={ToDo} />
-            <Card.ImgOverlay id='proyectsTxt'>
-              <h2>To do</h2>
-            </Card.ImgOverlay>
-          </Card>
+          <a href="https://to-do-delta-bice.vercel.app/" target={'_blank'}>
+            <Card id='proyects' style={{ width: '20rem' }}>
+              <Card.Img src={ToDo} />
+              <Card.ImgOverlay id='proyectsTxt'>
+                <h2>To do</h2>
+              </Card.ImgOverlay>
+            </Card>
+          </a>
         </Col>
       </Row>
     </Container>
