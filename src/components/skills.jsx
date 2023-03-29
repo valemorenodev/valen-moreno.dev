99015aeb3fe6e00b7712f96656ebb2376d8ca146
dianamorenodev <valentinamoreno2002@gmail.com>
2023-03-29 11:06:47 -0500

    Update skills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Container, Row, Image, Col } from 'react-bootstrap';
 import Laptop from '../assets/Laptop.png'
 import Palette from '../assets/Palette.png'
-import Carrusel from './carrusel';
 
 const Skills = () => {
   return (
@@ -29,8 +28,8 @@ const Skills = () => {
         <Col className='col-lg-8 col-md-8 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center' >
           <div className='skillSection'>
             <h4>Development</h4>
-            <span>Desarrolladora Front-end con mas 1 año de experiencia laboral certificable con HTML, CSS, JavaScript, TypeScript, Angular, React js, Boostrap y Taildwind.</span>
-            <span>Otras habilidades: metodologia agil Scrum, Docker, Portainer, mySql, Net Core, Sonarqube, Linux, ingreso maquinas virtuales de manera remota y manejo de variables de entorno.</span>
+            <span>Soy una desarrolladora Front-end con más de 1 año de experiencia laboral certificable. Tengo experiencia en el desarrollo de aplicaciones web utilizando tecnologías como HTML, CSS, JavaScript, TypeScript, Angular, React JS, Bootstrap y Tailwind.</span>
+            <span>Además, tengo habilidades en metodologías ágiles como Scrum, y en herramientas como Docker, Portainer, MySql, Net Core, Sonarqube, Linux, ingreso a máquinas virtuales de manera remota y manejo de variables de entorno.</span>
           </div>
         </Col>
       </Row>
@@ -38,7 +37,8 @@ const Skills = () => {
         <div className='col-lg-8 col-md-8 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center' >
           <div className='skillSection'>
             <h4>Design</h4>
-            <span>Fundamentos de UI/Desing & UX/Desing, manejo de Figma y Adobe XD</span>
+            <span>Tengo experiencia en fundamentos de UI/Design & UX/Design, y habilidades avanzadas en el manejo de Figma y Adobe XD. Con estas herramientas, he creado mockups y prototipos de alta calidad que han sido implementados con éxito en proyectos de diseño.</span>
+            <span>Además, tengo un buen ojo para el detalle y la estética, lo que me permite crear diseños funcionales y atractivos para los usuarios. Trabajo bien en equipo y tengo habilidades para la comunicación efectiva de mis ideas.</span>
           </div>
         </div>
         <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12 my-4 d-flex flex-col justify-content-center' >
